7998ad06c14f3956a70951c627e7fb2d8ea5401d
chinhnd <chinhnd@intrust.com.vn>
2023-11-21 14:54:06 +0700

    [chinhnd] change input to base64 string data

--- src/main/java/com/example/detail_cert/CertificateInfo.java
+++ src/main/java/com/example/detail_cert/CertificateInfo.java
@@ -1,23 +1,41 @@
 package com.example.detail_cert;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.MessageDigest;
+import java.util.Base64;
 
 public class CertificateInfo {
     public static void main(String[] args) {
         try {
-            // file path
-            String certificateFilePath = "./cert-files/chinhnd_issueCert.crt";
+//            // file path
+//            String certificateFilePath = "./cert-files/chinhnd_issueCert.crt";
+//
+//            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+//
+//            // Read cert from file
+//            FileInputStream fis = new FileInputStream(certificateFilePath);
+//            Certificate certificate = certificateFactory.generateCertificate(fis);
+//            fis.close();
+//
+//            X509Certificate x509Certificate = (X509Certificate) certificate;
 
+            // Certificate data in base64 string
+            String base64CertificateData = "MIIDmTCCAoGgAwIBAgIUaph30Kxq8Ch8d+hGuJYCiSXX+AowDQYJKoZIhvcNAQELBQAwGTEXMBUGA1UEAwwOSW50cnVzdENBIFRlc3QwHhcNMjMxMTA5MDM1ODM5WhcNMjUxMTA4MDM1ODM4WjBcMQswCQYDVQQGEwJWTjEUMBIGA1UEChMLSW50cnVzdG51bGwxGDAWBgNVBAMMD1BoYW4gVsSDbiBExaluZzEdMBsGCgmSJomT8ixkAQETDTEyMzQ1Njc4OTEyMzQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCS98GRA5lYCSKIpefic6q7kc2XXNSkAJ9wQergFlSaA5odXV7cjPZrx+UoQG8D4gauS9s4e2NVIEIO0fF03g8VjGi0oFXMj95QG0BaKANfrojG8RdKVCujYayKXIGW++mJUkpsT83Ss7KaFoIxkCe+Ju44Jed0PCyUUKToVj3jvIneIVlek7oD1OAtlwiMEnw1A1a1VvNbZU7OvXBnxHn2N6MU0YhaIOyqi101zkyxFnPAYUtdO96+Fqei3nphWP2MDv9brAAhl1fGfaMNV6+YEEi/LBaQtcnXtCfwwDA1QgItgIAB8ZPBzYLmoFoa2mk9HhDdxYJioh2j412mThq1AgMBAAGjgZUwgZIwDAYDVR0TAQH/BAIwADAfBgNVHSMEGDAWgBQfIxoYSfq+Y7KWsAEp2k7EocjiDTAyBgNVHSUEKzApBggrBgEFBQcDAgYIKwYBBQUHAwQGCSqGSIb3LwEBBQYIKwYBBQUHAwEwHQYDVR0OBBYEFIwsZRmOwNYIDiDXKyv5qP9/VWqTMA4GA1UdDwEB/wQEAwIF4DANBgkqhkiG9w0BAQsFAAOCAQEAkt1voO+Z0UYzv3Jgf+lp0j3kNFSlyRJNTuWcuCvr5bwWeEgA6ZL74ScgOCux2qSybH8T4bgbW7YKdwnahbrPvZfbkNcl0MFzHDOT/KF/63jw63RHuUquJWunMGecjR68in1C2AUH/HZlroEXlVnQz9RX0pWb+Jj72QK0jTR78bWkb3KZRB+sICUs+eWjD4jQgtDeHTF/hUwuPs/TFvyi+SPNX+7hTxdBG0KSlHEGwWqMPG4cTk3QJQf0Cj1V9F0wTPxDEYdTP9VrnyW6yXHd0JtohAPPkX+EvOn0LHdrjgk51KYstuz+RtnZ/INSXt7HpecHXftVqpsfxQsBBuJJBg==";
+
+            // Decode base64 string to byte array
+            byte[] certificateBytes = Base64.getDecoder().decode(base64CertificateData);
+
+            // Create a CertificateFactory for X.509
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
 
-            // Read cert from file
-            FileInputStream fis = new FileInputStream(certificateFilePath);
-            Certificate certificate = certificateFactory.generateCertificate(fis);
-            fis.close();
+            // Read cert from byte array
+            ByteArrayInputStream bis = new ByteArrayInputStream(certificateBytes);
+            Certificate certificate = certificateFactory.generateCertificate(bis);
+            bis.close();
 
             X509Certificate x509Certificate = (X509Certificate) certificate;
 
